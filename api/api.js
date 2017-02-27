95e0efb2194cd79fecb141f9634a0a1f5e2f10bf
Patrick Heneise <patrick@blended.io>
2017-02-27 12:23:25 +0100

    api init

---
+++ api/api.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+
+const Web3 = require('web3')
+const Koa = require('koa')
+const Router = require('koa-better-router')
+
+const web3 = new Web3(new Web3.providers.HttpProvider(`http://${process.env.ETH_HOST}:${process.env.ETH_PORT}`))
+const app = new Koa()
+let router = Router().loadMethods()
+
+router.get('/', require('./functions/main.js'))
+
+app.use(router.middleware())
+app.listen(3000)
