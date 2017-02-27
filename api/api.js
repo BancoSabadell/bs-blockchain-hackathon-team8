458e9edf307e4f03f80f01ac586ced99e08bec36
Patrick Heneise <patrick@blended.io>
2017-02-27 14:48:11 +0100

    wip

--- api/api.js
+++ api/api.js
@@ -1,11 +1,9 @@
 require('dotenv').config()
 
-const Web3 = require('web3')
 const Koa = require('koa')
 const Router = require('koa-better-router')
 const bodyParser = require('koa-bodyparser')
 
-const web3 = new Web3(new Web3.providers.HttpProvider(`http://${process.env.ETH_HOST}:${process.env.ETH_PORT}`))
 const app = new Koa()
 
 let router = Router().loadMethods()
--- api/functions/reserve.js
+++ api/functions/reserve.js
@@ -1,3 +1,6 @@
+const Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider(`http://${process.env.ETH_HOST}:${process.env.ETH_PORT}`))
+
 module.exports = exports = function (ctx) {
   let car = ctx.request.body.car
   let user = ctx.request.body.user
