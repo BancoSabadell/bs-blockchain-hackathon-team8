f6ba2842771f0f39a4c1e845d125e1bd42f67309
Patrick Heneise <patrick@blended.io>
2017-02-27 14:49:21 +0100

    feat: cors

--- api/api.js
+++ api/api.js
@@ -3,10 +3,12 @@ require('dotenv').config()
 const Web3 = require('web3')
 const Koa = require('koa')
 const Router = require('koa-better-router')
+const cors = require('kcors')
 
 const web3 = new Web3(new Web3.providers.HttpProvider(`http://${process.env.ETH_HOST}:${process.env.ETH_PORT}`))
 const app = new Koa()
 
+app.use(cors())
 let router = Router().loadMethods()
 
 router.get('/', require('./functions/main.js'))
