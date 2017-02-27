2a8f0a1a23dc5ab3221f41fedb213ea57293afc0
Patrick Heneise <patrick@blended.io>
2017-02-27 15:55:11 +0100

    fix: estimations

--- api/functions/estimation.js
+++ api/functions/estimation.js
@@ -3,26 +3,26 @@ const personas = require('../data/personas')
 const moment = require('moment')
 
 module.exports = exports = function (ctx) {
-  console.log(ctx.request)
   let from = moment(ctx.request.body.from)
   let to = moment(ctx.request.body.to)
   let hours = to.diff(from, 'hours')
 
   let car = cars.find(car => {
-    return car === ctx.request.body.car
+    return car.id === ctx.request.body.car
   })
 
   let renter = personas.find(persona => {
-    return persona === ctx.request.body.user
+    return persona.id === ctx.request.body.user
   })
 
-  let price = car.rentalPrice * (car.insuranceBase * renter.score) * hours
+  let price = (car.rentalPrice + (car.insuranceBase * renter.score)) * hours
 
   ctx.body = {
     from: from,
     to: to,
     car: car,
     renter: renter,
+    hours: hours,
     price: price
   }
 }
